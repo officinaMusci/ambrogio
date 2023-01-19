99831db91e3846d8788bee3fe002ecfc498f4bf7
officinaMusci <officina@musci.ch>
2023-01-19 14:40:16 +0100

    Add PyPI package long description

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+
+project_directory = Path(__file__).parent
+long_description = (project_directory / "README.md").read_text()
 
 setup(
     name='ambrogio',
     version='0.1.0',
+
     description='A simple framework to handle complex scripts.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     
     url='https://github.com/officinaMusci/ambrogio',
     project_urls={
