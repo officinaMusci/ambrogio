e843aa135dc8beaf614b5a1db446b41e9e10e70f
officinaMusci <officina@musci.ch>
2023-01-20 10:29:31 +0100

    Improve Timer class

--- ambrogio/utils/time.py
+++ ambrogio/utils/time.py
@@ -9,9 +9,16 @@ class Timer:
     beginning: float|None = None
     end: float|None = None
 
-    def __init__(self, start: float = True) -> None:
+    def __init__(self, start: bool = True) -> None:
         if start:
-          self.start()
+            self.start()
+
+    @property
+    def elapsed_time(self) -> str:
+        return self.format_elapsed_time(
+            self.beginning,
+            self.end or time.time()
+        )
 
     def start(self) -> None:
         self.beginning = time.time()
@@ -19,18 +26,12 @@ class Timer:
     def stop(self) -> None:
         self.end = time.time()
 
-    def get_elapsed_time(self) -> str:
-        return self.format_elapsed_time(
-            self.beginning,
-            self.end or time.time()
-        )
-
     @staticmethod
     def format_elapsed_time(start: float, end: float) -> str:
         hours, rem = divmod(end - start, 3600)
         minutes, seconds = divmod(rem, 60)
 
-        elapsed_time = "{:0>2}:{:0>2}:{:05.2f}".format(
+        elapsed_time = "{:0>2}:{:0>2}:{:05.3f}".format(
             int(hours),
             int(minutes),
             seconds
