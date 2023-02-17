0e794900be3d6bf9e1895141db679ffea53a1e11
Danilo Musci <danilomusci@gmail.com>
2023-02-17 15:32:31 +0100

    Update README

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (project_directory / "README.md").read_text()
 
 setup(
     name='ambrogio',
-    version='0.3.13',
+    version='0.4.0',
 
     description='A simple framework to handle complex scripts.',
     long_description=long_description,
