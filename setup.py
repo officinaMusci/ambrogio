d82c77e2e8c3be90c43a7dc2c0d2bd3b0130bcab
officinaMusci <danilo.musci@edu-vd.ch>
2023-07-25 13:00:41 +0200

    Update version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (project_directory / "README.md").read_text()
 
 setup(
     name='ambrogio',
-    version='0.6.7',
+    version='0.6.8',
 
     description='A simple framework to handle complex scripts.',
     long_description=long_description,
